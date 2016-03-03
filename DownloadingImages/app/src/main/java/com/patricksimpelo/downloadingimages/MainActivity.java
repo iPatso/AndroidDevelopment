f8e4c4dd230c74b2984ac92a6551d5e697aff71d
Patrick Simpelo <Patrick.simpelo@gmail.com>
2016-03-02 18:49:48 -0800

    Created and completed a simple app that grabs an image online via url and displays it on the screen

---
+++ DownloadingImages/app/src/main/java/com/patricksimpelo/downloadingimages/MainActivity.java
@@ -0,0 +1,81 @@
+package com.patricksimpelo.downloadingimages;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView downloadedImage;
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+    }
+
+    public void downloadImage(View view) {
+
+        // http://www.deadpoolcore.com/assets/images/image-deadpool.png
+
+        Log.i("Button Pressed", "Success");
+
+        ImageDownloader task = new ImageDownloader();
+        Bitmap myImage;
+
+        try {
+            myImage = task.execute("http://www.deadpoolcore.com/assets/images/image-deadpool.png").get();
+
+            downloadedImage.setImageBitmap(myImage);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        downloadedImage = (ImageView) findViewById(R.id.imageView);
+
+
+
+    }
+}
